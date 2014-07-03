a2b9a5a9a76e190cfcdb39dd4e8f7493a9d92199
sfusco <sfusco@ricin.local>
2014-07-03 04:42:44 -0400

    switching to prod url

--- app/scripts/services/apiconfig.js
+++ app/scripts/services/apiconfig.js
@@ -9,7 +9,6 @@
  */
 angular.module('twitterleaderboardApp')
   .constant('apiconfig', {
-    // rooturl: 'http://polar-eyrie-5877.herokuapp.com',
-    rooturl: 'http://localhost:5000',
+    rooturl: 'http://polar-eyrie-5877.herokuapp.com',
     twitterlogin: '/auth/twitter'
   });
