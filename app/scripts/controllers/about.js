6bf5323ef65ee7fefb67cdd6dfc6e9d7690a4770
sfusco <sfusco@ricin.local>
2014-07-17 00:02:08 -0400

    adding app icons and updated about page

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -19,5 +19,15 @@ angular.module('LinkboardApp')
       { icon: 'dashboard', text: 'Links shared on twitter are detected, counted and ranked' },
       { icon: 'heart', text: 'Discover new stuff to love :)' }
     ];
+
+    $scope.services = [
+      { href: 'https://twitter.com/twitter', name: 'twitter' },
+      { href: 'https://twitter.com/awscloud', name: 'amazon aws' },
+      { href: 'https://twitter.com/heroku', name: 'heroku' },
+      { href: 'https://twitter.com/getiron', name: 'iron.io' },
+      { href: 'https://twitter.com/mongohq', name: 'mongohq' },
+      { href: 'https://twitter.com/mongolab', name: 'mongolab' },
+      { href: 'https://twitter.com/RedisLabsInc', name: 'redis labs' },
+    ];
   })
 ;
