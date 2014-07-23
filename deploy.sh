935b2e89cec749cae9b6a4de0de1473a05f6aa06
sfusco <sfusco@ricin.local>
2014-07-22 23:56:12 -0400

    removing old deploy script

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-grunt
-#s3cmd -P -c .s3cfg sync ./dist/ s3://twitterleaderboards
-s3cmd -P -c .s3cfg sync ./dist/ s3://www.twitterleaderboards.com
-git tag -f $(date '+%Y-%m-%d')
